e94bff976379e3f53b1184586a7f41ebcea3be3c
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-10-16 17:24:35 -0200

    adding port environment variable

--- start.js
+++ start.js
@@ -4,8 +4,10 @@ var connect  = require('connect');
 var serverStatic = require('serve-static');
 var server = connect();
 var livereload = require('livereload');
+var port = process.env.PORT || 3000;
 
 server.use(serverStatic(__dirname + '/server'));
-server.listen(3000);
+server.listen(port);
 server = livereload.createServer();
 server.watch(__dirname + "/server");
+console.log('The app is running in http://localhost:' + port);
